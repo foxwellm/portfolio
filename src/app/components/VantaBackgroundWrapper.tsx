70b31a788553b8a746a94977ed532832db313de3
Matthew Foxwell <foxwellm@gmail.com>
2025-04-13 21:30:38 -0700

    refactor: vanta wrapper

--- src/app/components/VantaBackgroundWrapper.tsx
+++ src/app/components/VantaBackgroundWrapper.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import VantaBackground from "./VantaBackground";
 
-const VantaBackground = dynamic(() => import("./VantaBackground"), {
-  ssr: false, // Disable SSR to avoid "document is not defined"
-  loading: () => <div className="absolute top-0 left-0 w-full h-full z-0" />,
-});
-
-export default function HeroClientWrapper() {
-  return <VantaBackground />;
+export default function VantaBackgroundWrapper() {
+  const [isClientLoaded, setIsClientLoaded] = useState(false);
+  useEffect(() => {
+    setIsClientLoaded(true);
+  }, []);
+  return isClientLoaded ? <VantaBackground /> : <></>;
 }
