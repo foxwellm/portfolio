c79c81f0b29d0c295d7a45de021deaf0a8a47d42
Matthew Foxwell <foxwellm@gmail.com>
2025-04-18 04:46:58 -0700

    refactor: navbar responsive

--- src/app/sections/StickyNavbar.tsx
+++ src/app/sections/StickyNavbar.tsx
@@ -1,13 +1,11 @@
-'use client'
+"use client";
 
 import { useActiveSection } from "../../../hooks/useActiveSection";
-import { useIsMobile } from "../../../hooks/useIsMobile";
 
 const sections = ["home", "about", "projects", "contact"];
 
 export default function Navbar() {
   const activeSection = useActiveSection(sections);
-  const isMobile = useIsMobile()
 
   return (
     <nav className="sticky bg-black h-16 top-0 z-20 text-white px-6 flex justify-center items-center gap-16">
@@ -15,8 +13,10 @@ export default function Navbar() {
         <a
           key={section}
           href={`#${section}`}
-          className={`transition ${isMobile ? "text-sm" : "text-lg"} ${
-            activeSection === section ? "text-sky-400 scale-110" : "hover:text-gray-300"
+          className={`transition lg:text-sm lg:text-xl ${
+            activeSection === section
+              ? "text-sky-400 scale-110"
+              : "hover:text-gray-300"
           }`}
         >
           {section.charAt(0).toUpperCase() + section.slice(1)}
