8be6d4506802a5bdb7a80505341db7dc421279e8
Matthew Foxwell <foxwellm@gmail.com>
2025-04-25 09:09:00 -0700

    refactor: chatbox responsive

--- src/app/sections/Chatbot/ChatBox.tsx
+++ src/app/sections/Chatbot/ChatBox.tsx
@@ -4,8 +4,15 @@ import { useState, useEffect, useRef, forwardRef } from "react";
 
 const ChatBox = forwardRef<HTMLDivElement, { isChatOpen: boolean }>(
   ({ isChatOpen }, ref) => {
-    const [messages, setMessages] =
-      useState<{ role: string; content: string }[]>([]);
+    const [messages, setMessages] = useState<
+      { role: string; content: string }[]
+    >([
+      {
+        role: "bot",
+        content:
+          "Hello! Welcome to Matthew Foxwell's portfolio. How can I assist you today?",
+      },
+    ]);
     const [input, setInput] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const messagesEndRef = useRef<HTMLDivElement>(null);
@@ -23,10 +30,15 @@ const ChatBox = forwardRef<HTMLDivElement, { isChatOpen: boolean }>(
         body: JSON.stringify({ message: input }),
       });
 
-      const data = await response.json();
-      const botMessage = { role: "bot", content: data.reply };
-      setIsLoading(false);
-      setMessages((prev) => [...prev, botMessage]);
+      if (!response.ok) {
+        // TODO: Handle UI Error
+        setIsLoading(false);
+      } else {
+        const data = await response.json();
+        const botMessage = { role: "bot", content: data.reply };
+        setIsLoading(false);
+        setMessages((prev) => [...prev, botMessage]);
+      }
     };
 
     useEffect(() => {
@@ -36,19 +48,19 @@ const ChatBox = forwardRef<HTMLDivElement, { isChatOpen: boolean }>(
     return (
       <div
         ref={ref}
-        className={`relative bottom-[1rem] right-[1rem] w-full max-w-[500px] z-50 transition-all duration-300 ease-in-out  
-    ${isChatOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"}`}
+        className={`relative bottom-4 right-4 w-full max-w-[calc(100vw-2rem)] sm:max-w-130 z-50 transition-all duration-300 ease-in-out  
+    ${isChatOpen ? "max-h-120 opacity-100" : "max-h-0 opacity-0"}`}
       >
         <div className="bg-gray-900 rounded-xl shadow-lg flex flex-col border border-gray-300 border-gray-700 overflow-hidden">
           {/* Chat messages */}
-          <div className="overflow-y-auto max-h-[300px] p-4 gap-2 flex flex-col">
+          <div className="overflow-y-auto max-h-80 p-4 gap-2 flex flex-col">
             {messages.map((msg, idx) => (
               <div
                 key={idx}
                 className={`p-3 max-w-[80%] rounded-lg text-sm text-black ${
                   msg.role === "user"
-                    ? "bg-blue-100 self-end text-right"
-                    : "bg-gray-100 self-start text-left"
+                    ? "bg-blue-200 self-end text-left"
+                    : "bg-gray-200 self-start text-left"
                 }`}
               >
                 {msg.content}
@@ -57,7 +69,7 @@ const ChatBox = forwardRef<HTMLDivElement, { isChatOpen: boolean }>(
             {isLoading && (
               <div className="min-h-[3rem] w-[80%] rounded-lg bg-gray-400 animate-pulse" />
             )}
-            <div ref={messagesEndRef} />
+            <div ref={messagesEndRef} className="-mt-2" />
           </div>
 
           {/* Input bar */}
@@ -65,7 +77,7 @@ const ChatBox = forwardRef<HTMLDivElement, { isChatOpen: boolean }>(
             <input
               type="text"
               className="flex-1 p-2 border border-gray-300 dark:border-gray-600 rounded"
-              placeholder="Ask Matthew's chatbot something..."
+              placeholder="Ask something..."
               value={input}
               onChange={(e) => setInput(e.target.value)}
               onKeyDown={(e) => e.key === "Enter" && sendMessage()}
@@ -73,6 +85,7 @@ const ChatBox = forwardRef<HTMLDivElement, { isChatOpen: boolean }>(
             <button
               onClick={sendMessage}
               className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              disabled={isLoading}
             >
               Send
             </button>
