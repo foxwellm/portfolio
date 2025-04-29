bc41f491fcbb351499d043d74b959059ef2518b4
Matthew Foxwell <foxwellm@gmail.com>
2025-04-29 03:20:27 -0700

    refactor: vanta document check

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useActiveSection } from "./useActiveSection";
-export { useDocumentReady } from "./useDocumentReady";
 export { useObserveThreshold } from "./useObserveThreshold";
 export { useWindowResize } from "./useWindowResize";
