28a4def2be3940097049929d1135e95b61bd362b
Matthew Foxwell <foxwellm@gmail.com>
2025-06-08 06:57:12 -0700

    refactor: social links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="flex flex-col items-center justify-evenly py-4 bg-gray-950 h-36 border-t border-white/10 text-gray-400 text-sm">
       <div className="flex gap-12">
         <a
-          href={process.env.GITHUB}
+          href="https://github.com/foxwellm/portfolio"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 hover:text-sky-400 transition"
@@ -19,22 +19,27 @@ export function Footer() {
           <span>GitHub</span>
         </a>
         <a
-          href={process.env.LINKEDIN}
+          href="https://linkedin.com/in/foxwellmw"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 hover:text-sky-400 transition"
         >
-          <Image src="/footer/InBug-White.png" alt="LinkedIn" width={28} height={28} />
+          <Image
+            src="/footer/InBug-White.png"
+            alt="LinkedIn"
+            width={28}
+            height={28}
+          />
           <span>LinkedIn</span>
         </a>
       </div>
-        <a
-          href="https://www.flaticon.com/free-icons/chatbot"
-          title="chatbot icons"
-          className="mt-4"
-        >
-          Chatbot icons created by littleicon - Flaticon
-        </a>
+      <a
+        href="https://www.flaticon.com/free-icons/chatbot"
+        title="chatbot icons"
+        className="mt-4"
+      >
+        Chatbot icons created by littleicon - Flaticon
+      </a>
 
       <p className="text-center mt-4">
         &copy; {new Date().getFullYear()} Matthew Foxwell. All rights reserved.
